6d0b1804bec98bbef077bba15fc2b79de60f4d54
Prash Mohan <prashmohanuon@gmail.com>
2020-06-13 23:40:28 +0100

    player styling

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-const grids = document.querySelectorAll('element div')
-const result = document.querySelector('#result')
-const currentPlayer = doucment.querySelector('#current-player')
+document.addEventListener('DOMContentLoaded', () => {
+
+  const grids = document.querySelectorAll('element div')
+  const result = document.querySelector('#result')
+  const displayCurrentPlayer = document.querySelector('#current-player')
+  let currentPlayer = 1
+
+})
