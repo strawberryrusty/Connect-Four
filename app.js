91e24890dd4e9762d6cb2856ead4fda08ecb689b
Prash Mohan <prashmohanuon@gmail.com>
2020-06-14 23:48:48 +0100

    notes in JS updated

--- app.js
+++ app.js
@@ -9,24 +9,27 @@ document.addEventListener('DOMContentLoaded', () => {
   for (var i = 0; i < grids.length; i++)
 
     (function(index){
-    //add an onclick to each square in your grid
+    //here we are adding an onclick to each square in your grid
       grids[i].onclick = function(){
-      //if the square below your current square is taken, you can go ontop of it
+
+      //logic if the grid below your current square is taken, you can go ontop of it
         if(grids[index + 7].classList.contains('taken')){
           if (currentPlayer === 1) {
             grids[index].classList.add('taken')
             grids[index].classList.add('player-one')
+
             //change the player
             currentPlayer = 2
             displayCurrentPlayer.innerHTML = currentPlayer
           } else if (currentPlayer === 2) {
             grids[index].classList.add('taken')
             grids[index].classList.add('player-two')
+
             //change the player
             currentPlayer = 1
             displayCurrentPlayer.innerHTML = currentPlayer
           }
-          //if the sqaure below your current swqaure is not taken, you can't go there
+          //if the grid below your current grid is not taken, you can't go there
         } else alert('cant go here')
       }
     })(i)
