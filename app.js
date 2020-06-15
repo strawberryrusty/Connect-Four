7e79d67005951bf9c14615d99f810ab9b88fe5cb
Prash Mohan <prashmohanuon@gmail.com>
2020-06-15 23:41:49 +0100

    JS notes updated, working code

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+//all the JS code is done through this event listener
 document.addEventListener('DOMContentLoaded', () => {
 
   const grids = document.querySelectorAll('.element div')
@@ -5,20 +7,23 @@ document.addEventListener('DOMContentLoaded', () => {
   const displayCurrentPlayer = document.querySelector('#current-player')
   let currentPlayer = 1
 
-
+  //loop through each square
   for (var i = 0; i < grids.length; i++)
 
     (function(index){
-    //here we are adding an onclick to each square in your grid
+    //here we are adding an onclick function everytime we click a square in our grid
       grids[i].onclick = function(){
 
-      //logic if the grid below your current square is taken, you can go ontop of it
+      // if the grid below your current grid or 7 squares below has a classname of taken , you are allowed to click on this grid and claim it as your taken and can go.
         if(grids[index + 7].classList.contains('taken')){
+
+          //if you can go and are player 1, add classname taken and playerone to that
+          //square
           if (currentPlayer === 1) {
             grids[index].classList.add('taken')
             grids[index].classList.add('player-one')
 
-            //change the player
+            //change the player (do the same for player 2)
             currentPlayer = 2
             displayCurrentPlayer.innerHTML = currentPlayer
           } else if (currentPlayer === 2) {
